1a872a0e9afabc938332fd99242641f0eb92273a
Ian <isabado@onpointglobal.com>
2022-09-21 22:55:31 -0700

    testing commit

---
+++ drink-menu-app/src/App.vue
@@ -0,0 +1,50 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="main-container">
+
+    <div class="img-container">
+     <div class="box img-item"></div>
+    </div>
+
+    <div class="search-container box">
+      <div class="box query1">
+        <h3>Search for a Drink by name</h3>
+      </div>
+      <div class="box query2">
+       <h3>More Information on Alcohol</h3>
+      </div>
+      <div class="box query3">
+        Generate a Random Drink
+      </div>
+    </div>
+
+
+
+  </div>
+
+  
+</template>
+
+<style scoped>
+ .img-container {
+  display: flex;
+  justify-content: center;
+  height: 40vh;
+ }
+
+ .img-item {
+  width: 60vw;
+ }
+
+ .search-container {
+  margin-top: 20px;
+  display: grid;
+  justify-content: center;
+  
+
+
+ }
+</style>
